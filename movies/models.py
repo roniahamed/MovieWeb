9e11649379493938377c98b057d620aecd300dfc
roniahamed <mdroniahamed56@gmail.com>
2025-04-09 16:58:28 +0600

    update

--- movies/models.py
+++ movies/models.py
@@ -5,3 +5,7 @@ class MovieData(models.Model):
     name = models.CharField(max_length=200)
     duration = models.FloatField()
     rating = models.FloatField()
+
+
+    def __str__(self):
+        return self.name 
