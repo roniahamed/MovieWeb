40cb4de95ffa06a4ab80403de64a9e1d2d63dc86
roniahamed <mdroniahamed56@gmail.com>
2025-04-14 20:26:14 +0600

    MoviData Table updated

--- movies/models.py
+++ movies/models.py
@@ -5,6 +5,8 @@ class MovieData(models.Model):
     name = models.CharField(max_length=200)
     duration = models.FloatField()
     rating = models.FloatField()
+    typ = models.CharField(max_length=200, default='action')
+    image = models.ImageField(upload_to='Images', default='Images/None/noimg.jpg')
 
 
     def __str__(self):
