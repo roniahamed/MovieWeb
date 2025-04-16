f099b8be5f0ae1ff3cff7d212dfdb08bd0968d33
roniahamed <mdroniahamed56@gmail.com>
2025-04-16 19:36:54 +0600

    update

--- MovieWeb/urls.py
+++ MovieWeb/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from movies.views import MovieViewSet, ActionViewSet, ComedyMovie
+from django.conf.urls.static import static 
+from django.conf import settings 
 
 router = routers.DefaultRouter()
 router.register('movies', MovieViewSet, basename='movidata')
@@ -27,4 +29,4 @@ router.register('comedy', ComedyMovie, basename='comedy')
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
